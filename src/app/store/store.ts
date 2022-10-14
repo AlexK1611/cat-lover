3c281bcc59fc47b588adf3b73b6854c304759e50
AlexK1611 <oleksandr.kharchenko@computools.com>
2022-10-14 16:25:50 +0300

    app types update

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -3,6 +3,3 @@ import { configureStore } from '@reduxjs/toolkit'
 export const store = configureStore({
     reducer: {}
 })
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
